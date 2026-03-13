9f72849dfcfbcf5317fd1b2d3ad69fd85e8ec7d3
alvinkodes <alvinkok3064@gmail.com>
2026-03-13 18:11:03 +0800

    first uniq char

---
+++ firstUniqChar/first_uniq_char.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+char first_uniq_char(char *s)
+{
+	char counts[128] = {0};
+
+	for (size_t i = 0; s[i] != '\0'; i++) {
+		counts[s[i]] += 1;
+	}
+	for (size_t j = 0; s[j] != '\0'; j++) {
+		if (counts[s[j]] == 1) {
+			return (s[j]);
+		}
+	}
+	return ('\0');
+}
+
+int main(void)
+{
+	struct test_case {
+		char *input;
+		char expected;
+	} cases[] = {
+		{"parallel", 'p'},
+		{"aabb", '\0'},
+		{"aabbc", 'c'},
+		{"z", 'z'},
+		{"", '\0'},
+		{"ajoifooafvb", 'j'},
+		{"ggggggggg", '\0'},
+	};
+
+	size_t size = sizeof(cases) / sizeof(cases[0]);
+
+	printf("%-14s | %-8s | %-8s\n", "String", "Result", "Expected");
+	printf("%-14s | %-8s | %-8s\n", "--------------", "--------", "--------");
+
+	for (size_t i = 0; i < size; i++) {
+		char result = first_uniq_char(cases[i].input);
+
+		printf("%-14s | ", cases[i].input);
+		if (result == '\0') {
+			printf("%-8s", "\\0");
+		} else {
+			printf("%-8c", result);
+		}
+		printf(" | ");
+		if (cases[i].expected == '\0') {
+			printf("%-8s\n", "\\0");
+		} else {
+			printf("%-8c\n", cases[i].expected);
+		}
+	}
+
+	return (0);
+}
